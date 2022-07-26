152b51c59519847187f89111cbf19728357a0166
Nihalroshan <nihalroshanpc@gmail.com>
2022-07-26 11:15:15 +0530

    starter files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div className='grid place-items-center'>Go Sports</div>
